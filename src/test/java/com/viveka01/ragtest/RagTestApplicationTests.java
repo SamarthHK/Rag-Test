e26b287741b2e1d0778406d097436a7f77c96d48
Munna <samarthHKPersonal@outlook.com>
2026-06-28 19:06:18 -0700

    initial commit

---
+++ src/test/java/com/viveka01/ragtest/RagTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viveka01.ragtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RagTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
